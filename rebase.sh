249adba6ca518aa6c930af3898e59d0b9c3cfdab
Jeff Dean <jeff@galvanize.com>
2017-06-25 16:53:50 -0600

    fixed rebase

--- rebase.sh
+++ rebase.sh
@@ -5,18 +5,18 @@ set -e
 git checkout master
 git push
 
-git checkout shopping-cart-v01
+git checkout cart-01-header-footer
 git rebase master
-git push origin shopping-cart-v01 --force-with-lease
+git push -u origin cart-01-header-footer --force-with-lease
 
-git checkout shopping-cart-v02
-git rebase shopping-cart-v01
-git push origin shopping-cart-v02 --force-with-lease
+git checkout cart-02-props
+git rebase cart-01-header-footer
+git push -u origin cart-02-props --force-with-lease
 
-git checkout shopping-cart-v03
-git rebase shopping-cart-v02
-git push origin shopping-cart-v03 --force-with-lease
+git checkout cart-03-item-list
+git rebase cart-02-props
+git push -u origin cart-03-item-list --force-with-lease
 
-git checkout shopping-cart-v04
-git rebase shopping-cart-v03
-git push origin shopping-cart-v04 --force-with-lease
+git checkout cart-04-add-item
+git rebase cart-03-item-list
+git push -u origin cart-04-add-item --force-with-lease
