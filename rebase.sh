13070d04db3d21fef5f0e45a9908fdf155e651a2
Jeff Dean <jeff@galvanize.com>
2017-06-25 23:24:24 -0600

    update rebase script

--- rebase.sh
+++ rebase.sh
@@ -20,3 +20,11 @@ git push -u origin cart-03-item-list --force-with-lease
 git checkout cart-04-add-item
 git rebase cart-03-item-list
 git push -u origin cart-04-add-item --force-with-lease
+
+git checkout cart-05-calculate-total
+git rebase cart-04-add-item
+git push -u origin cart-05-calculate-total --force-with-lease
+
+git checkout cart-06-deploy
+git rebase cart-05-calculate-total
+git push -u origin cart-06-deploy --force-with-lease
