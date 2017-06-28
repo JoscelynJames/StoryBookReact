73e937459b8d82d4fcee30c032acb58e30c98df0
Jeff Dean <jeff@galvanize.com>
2017-06-28 01:50:08 -0600

    async / await work in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
+import 'babel-polyfill';
 import { configure } from '@storybook/react';
 
 function loadStories() {
