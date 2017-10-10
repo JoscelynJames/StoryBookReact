1c9d0d32a479af66c719def3c1320fd8b20efcce
Joscelyn James <josce.james7@hotmail.com>
2017-10-10 14:46:11 -0600

    added timer component

--- src/stories/index.js
+++ src/stories/index.js
@@ -7,8 +7,12 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import Message from '../components/Message';
+import Timer from '../components/Timer';
 
 storiesOf('Message', module)
   .add('Hello World', () => <Message text="Hello world" />)
   .add("What's the time?", () => <Message text={new Date().toString()} />)
   .add('poptarts', () => <Message text="poptarts"/>)
+
+storiesOf('Timer', module)
+  .add('Show', () => <Timer />)
